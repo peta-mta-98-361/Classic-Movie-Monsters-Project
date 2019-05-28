e560c758737d7d00d99b51dbe0c34beab4141ccf
mikechittenden1974 <47178657+mikechittenden1974@users.noreply.github.com>
2019-05-28 14:28:52 +0100

    Add files via upload

---
+++ ClassicMovieMonsterProject/Monster.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassicMovieMonsterProject
+{
+    class Monster
+    {
+        //data members
+        private string name;
+        private int strength;
+        private int cunning;
+        private int yearofrelease;
+        private string actor;
+        private string features;
+        //properties
+        //constructors
+        public Monster(string n, int s, int c, int y, string a, string f)
+        {
+            name = n;
+            strength = s;
+            cunning = c;
+            yearofrelease = y;
+            actor = a;
+            features = f;
+        }
+        //methods
+        public void Display()
+        {
+            Console.WriteLine("Monster Name:\t\t{0}\nFeatures:\t\t{1}", name, features);
+            Console.WriteLine("Strength:\t\t{0}\nCunning:\t\t{1}", strength, cunning);
+            Console.WriteLine("Year of release:\t{0}", yearofrelease);
+            Console.WriteLine("Actor:\t\t\t{0}",actor);
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ClassicMovieMonsterProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassicMovieMonsterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster Frank = new Monster("Frankenstein's Monster", 97, 12, 1931, "Boris Karloff", "Golem");
+            Frank.Display();
+
+            Monster Dracula = new Monster("Dracula", 80, 80, 1931, "Bela Lugosi","Undead parasite");
+            Dracula.Display();
+
+            Monster Wolfie = new Monster("The Wolfman", 90, 30, 1941, "Lon Chaney Jr", "Lycanthrope");
+            Dracula.Display();
+
+            Monster Phantom = new Monster("The Phantom of the Opera", 44, 79, 1925, "Lon Chaney", "Manipulation");
+            Phantom.Display();
+
+            Monster Hyde = new Monster("Dr. Jekyll and Mr. Hyde", 97, 15, 1913, "King Baggot","Metamorphosis");
+            Hyde.Display();
+        }
+    }
+}
